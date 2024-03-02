b1e59d3459e6218c7e6ca36dde37ca3270eded03
CAKB2ENS <147879829+CAKB2ENS@users.noreply.github.com>
2024-03-03 00:23:13 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,9 +1,6 @@
 
 class ProjectAgent:   
     def __init__(self):
-        import os
-        import torch
-        import torch.nn as nn
         self.model = []
         self.device = 'cpu'
     
@@ -14,12 +11,9 @@ class ProjectAgent:
         pass
 
     def load(self):
-        # import os
-        # import torch
         self.model = torch.load(os.path.join(os.getcwd(),'Model_v1_PatientRef_E500.pth'), map_location=self.device)
 
     def greedy_action(self, observation):
-        # import torch
         with torch.no_grad():
             Q = self.model(torch.Tensor(observation).unsqueeze(0).to('cpu'))
             return torch.argmax(Q).item()

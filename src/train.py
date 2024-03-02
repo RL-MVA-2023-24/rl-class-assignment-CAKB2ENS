8d43bf3758a4cb24b6a460629b1aac02539b38ce
CAKB2ENS <147879829+CAKB2ENS@users.noreply.github.com>
2024-03-03 00:12:31 +0100

    Test sur le fichier "train"

--- src/train.py
+++ src/train.py
@@ -1,20 +1,9 @@
-from gymnasium.wrappers import TimeLimit
-from env_hiv import HIVPatient
 
-import os
-import torch
-
-env = TimeLimit(
-    env=HIVPatient(domain_randomization=False), max_episode_steps=200
-)  # The time wrapper limits the number of steps in an episode at 200.
-# Now is the floor is yours to implement the agent and train it.
-
-
-# You have to implement your own agent.
-# Don't modify the methods names and signatures, but you can add methods.
-# ENJOY!
 class ProjectAgent:   
     def __init__(self):
+        import os
+        import torch
+        import torch.nn as nn
         self.model = []
         self.device = 'cpu'
     

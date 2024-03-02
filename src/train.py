1b1d15c9f3326b3dd1b8499ea919bdcd95867d55
CAKB2ENS <147879829+CAKB2ENS@users.noreply.github.com>
2024-03-02 23:57:27 +0100

    Utilisation d'un train.py simplifié
    Je dépose une version très simplifiée du fichier "train.py" pour tester le processus de notation

--- src/train.py
+++ src/train.py
@@ -1,6 +1,9 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
 
+import os
+import torch
+
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
@@ -10,12 +13,27 @@ env = TimeLimit(
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
-class ProjectAgent:
+class ProjectAgent:   
+    def __init__(self):
+        self.model = []
+        self.device = 'cpu'
+    
     def act(self, observation, use_random=False):
-        return 0
+        return self.greedy_action(observation)
 
     def save(self, path):
         pass
 
     def load(self):
-        pass
+        # import os
+        # import torch
+        self.model = torch.load(os.path.join(os.getcwd(),'Model_v1_PatientRef_E500.pth'), map_location=self.device)
+
+    def greedy_action(self, observation):
+        # import torch
+        with torch.no_grad():
+            Q = self.model(torch.Tensor(observation).unsqueeze(0).to('cpu'))
+            return torch.argmax(Q).item()
+
+
+
